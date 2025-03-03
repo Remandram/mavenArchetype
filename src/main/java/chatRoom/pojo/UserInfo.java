09bc92f487fb2652a64871ed258ac50b166a3f78
Remandram <2471043361@qq.com>
2025-03-03 21:31:33 +0800

    pojo层UserInfo类,对应mysql中的User表

--- src/main/java/chatRoom/pojo/UserInfo.java
+++ src/main/java/chatRoom/pojo/UserInfo.java
@@ -1,8 +1,16 @@
 package chatRoom.pojo;
 import lombok.Data;
 
+/*
+* pojo(model)层
+* 设计数据结构,每一个pojo类中的各变量对应数据库中一张表的各字段
+* 例如这个UserInfo类就对应了数据库中的User表
+* */
+
+
+
 @Data
-public class user {
+public class UserInfo {
     // 用户名
     private String username;
     // 用户邮箱
