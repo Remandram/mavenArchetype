da78d8acc154b0a699f36b198ce15db443f0b74e
Remandram <2471043361@qq.com>
2025-03-04 17:00:40 +0800

    pojo层 UserInfo类

--- src/main/java/chatRoom/pojo/UserInfo.java
+++ src/main/java/chatRoom/pojo/UserInfo.java
@@ -8,15 +8,13 @@ import lombok.Data;
 * */
 
 
-
 @Data
 public class UserInfo {
+    private String userid;
     // 用户名
     private String username;
     // 用户邮箱
     private String email;
-    // 用户手机号
-    private String telephone;
     // 用户密码(应该是选填)
     private String password;
 }
