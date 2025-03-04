a2c4342e58186314026ff542271e0b3d22016037
Remandram <2471043361@qq.com>
2025-03-04 16:59:20 +0800

    测试mavenAt数据库连接是否成功的方法

--- src/main/java/test/MysqlConnectionTest.java
+++ src/main/java/test/MysqlConnectionTest.java
@@ -10,9 +10,24 @@ public class MysqlConnectionTest {
         Connection conn = JDBCUtils.getConnection();
         String sql = "select * from User";
         try{
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println(resultSet);
+            PreparedStatement preparedStatement = null;
+            if (conn != null) {
+                preparedStatement = conn.prepareStatement(sql);
+            }
+            ResultSet resultSet = null;
+            if (preparedStatement != null) {
+                resultSet = preparedStatement.executeQuery();
+            }
+            if (resultSet != null) {
+                while (resultSet.next()) {
+                    String id = resultSet.getString("userid");
+                    String username = resultSet.getString("username");
+                    String password = resultSet.getString("password");
+                    String useremail = resultSet.getString("useremail");
+                    System.out.println("userid:" + id + "\tusername:" + username + "\tpassword:" + password + "\tuseremail:" + useremail);
+                }
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
