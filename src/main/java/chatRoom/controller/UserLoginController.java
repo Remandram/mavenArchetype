559fb7c1b21c60a4d64c82e2b7809a21a2cc53ec
Remandram <2471043361@qq.com>
2025-03-04 17:01:06 +0800

    Controller层 测试,暂未实现

---
+++ src/main/java/chatRoom/controller/UserLoginController.java
@@ -0,0 +1,17 @@
+package chatRoom.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ccc/ddd")
+public class UserLoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
